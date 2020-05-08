7602d135563c9e1a55df063b890b80ef16845b3c
arnoldpring <arnold.xpanse@gmail.com>
2020-05-08 19:20:22 +0800

    una

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      width: {
+        '90%': '90%',
+        '40.7%': '40.7%',
+        '25rem': '25rem',
+      },
+      margin: {
+        '7.7':'7.7rem',
+      },
+      height: {
+        '670px': '670px',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
